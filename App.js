104b8942f111aa6c7d8aad22f06644a2411bb357
eungangku <eungangku@gmail.com>
2022-05-20 09:52:18 +0900

    deploy web

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert, Platform } from "react-native";
 import { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialIcons } from "@expo/vector-icons";
 import { theme } from "./colors";
 
-STORAGE_KEY = "@todos";
+const STORAGE_KEY = "@todos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
@@ -20,28 +20,39 @@ export default function App() {
   };
   const loadTodos = async () => {
     const loadedTodos = await AsyncStorage.getItem(STORAGE_KEY);
-    console.log(loadedTodos);
-    setTodos(JSON.parse(loadedTodos));
+    if (loadedTodos) {
+      setTodos(JSON.parse(loadedTodos));
+    }
   };
-  const addTodo = async () => {
+  const addTodo = async (item) => {
     const newTodo = Object.assign({}, { [Date.now()]: { text, working, finish: false } }, todos);
     setTodos(newTodo);
     await saveTodos(newTodo);
     setText("");
+    item.target.focus();
   };
   const deleteTodo = (key) => {
-    Alert.alert("Delete Todo", "Are you sure?", [
-      { text: "Cancel" },
-      {
-        text: "Delete",
-        onPress: () => {
-          const newTodo = { ...todos };
-          delete newTodo[key];
-          setTodos(newTodo);
-          saveTodos(newTodo);
+    if (Platform.OS === "web") {
+      if (window.confirm("Are you sure you want to delete?")) {
+        const newTodo = { ...todos };
+        delete newTodo[key];
+        setTodos(newTodo);
+        saveTodos(newTodo);
+      }
+    } else {
+      Alert.alert("Delete Todo", "Are you sure?", [
+        { text: "Cancel" },
+        {
+          text: "Delete",
+          onPress: () => {
+            const newTodo = { ...todos };
+            delete newTodo[key];
+            setTodos(newTodo);
+            saveTodos(newTodo);
+          },
         },
-      },
-    ]);
+      ]);
+    }
   };
   const finishTodo = (key) => {
     if (todos[key].finish === true) {
@@ -65,13 +76,13 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
-          <Text style={{ ...styles.btnText, color: working ? "white" : theme.gray }}>Work</Text>
+          <Text style={{ fontSize: 38, fontWeight: "600", color: working ? "white" : theme.gray }}>Work</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={travel}>
-          <Text style={{ ...styles.btnText, color: working ? theme.gray : "white" }}>Travel</Text>
+          <Text style={{ fontSize: 38, fontWeight: "600", color: working ? theme.gray : "white" }}>Travel</Text>
         </TouchableOpacity>
       </View>
-      <TextInput onSubmitEditing={addTodo} onChangeText={onChangeText} value={text} placeholder={working ? "What are you going to do?" : "Where do you want to travel?"} style={styles.input}></TextInput>
+      <TextInput autoFocus={true} onSubmitEditing={addTodo} onChangeText={onChangeText} value={text} placeholder={working ? "What are you going to do?" : "Where do you want to travel?"} style={styles.input}></TextInput>
       <ScrollView>
         {Object.keys(todos).map((key) =>
           working === todos[key].working ? (
@@ -86,7 +97,9 @@ export default function App() {
                 </TouchableOpacity>
               </View>
             </View>
-          ) : null
+          ) : (
+            <View></View>
+          )
         )}
       </ScrollView>
       <StatusBar style="light" />
@@ -104,10 +117,6 @@ const styles = StyleSheet.create({
     marginTop: 100,
     flexDirection: "row",
     justifyContent: "space-between",
-  },
-  btnText: {
-    fontSize: 38,
-    fontWeight: "600",
   },
   input: {
     backgroundColor: "white",
